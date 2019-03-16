91ffe6378d5c92b4bc2f5fcb97854163fcd3d5f5
Alex Ngun <ukmerryxd@gmail.com>
2019-03-16 00:34:50 -0700

    change the objects name for better readibility

--- GradeCalculator/gradecalculator.cpp
+++ GradeCalculator/gradecalculator.cpp
@@ -8,9 +8,13 @@ GradeCalculator::GradeCalculator(QWidget *parent) :
     ui->setupUi(this);
     ui->lcdNumber->setDigitCount(4);
     ui->lcdNumber->setPalette(Qt::red);
+    ui->stackedWidget->setCurrentIndex(0);
 
-    QObject::connect(ui->horizontalSlider,SIGNAL(valueChanged(int)),
-                         ui->spinBox, SLOT(setValue(int)));
+    QObject::connect(ui->comboBox, SIGNAL(currentIndexChanged(int)),
+                         ui->stackedWidget, SLOT(setCurrentIndex(int)));
+
+    QObject::connect(ui->horizontalSlider_1,SIGNAL(valueChanged(int)),
+                         ui->spinBox_1, SLOT(setValue(int)));
     QObject::connect(ui->horizontalSlider_2,SIGNAL(valueChanged(int)),
                          ui->spinBox_2, SLOT(setValue(int)));
     QObject::connect(ui->horizontalSlider_3,SIGNAL(valueChanged(int)),
@@ -19,15 +23,13 @@ GradeCalculator::GradeCalculator(QWidget *parent) :
                          ui->spinBox_4, SLOT(setValue(int)));
     QObject::connect(ui->horizontalSlider_5,SIGNAL(valueChanged(int)),
                          ui->spinBox_5, SLOT(setValue(int)));
-    QObject::connect(ui->horizontalSlider_6,SIGNAL(valueChanged(int)),
-                         ui->spinBox_6, SLOT(setValue(int)));
-    QObject::connect(ui->horizontalSlider_7,SIGNAL(valueChanged(int)),
-                         ui->spinBox_7, SLOT(setValue(int)));
-    QObject::connect(ui->horizontalSlider_8,SIGNAL(valueChanged(int)),
-                         ui->spinBox_8, SLOT(setValue(int)));
+    QObject::connect(ui->horizontalSlider_midterm1,SIGNAL(valueChanged(int)),
+                         ui->spinBox_midterm1, SLOT(setValue(int)));
+    QObject::connect(ui->horizontalSlider_final,SIGNAL(valueChanged(int)),
+                         ui->spinBox_final, SLOT(setValue(int)));
+    QObject::connect(ui->horizontalSlider_project,SIGNAL(valueChanged(int)),
+                         ui->spinBox_project, SLOT(setValue(int)));
 
-    QObject::connect(ui->comboBox, SIGNAL(currentIndexChanged(int)),
-                         ui->stackedWidget, SLOT(setCurrentIndex(int)));
 
     QObject::connect(ui->radioButton, SIGNAL(clicked()),
                          this, SLOT(update_overall()));
@@ -53,34 +55,34 @@ double GradeCalculator::compute_overall()
 
     if(ui->radioButton->isChecked())
     {
-        overall += 0.15*(ui->spinBox->value() +
+        overall += 0.15*(ui->spinBox_1->value() +
                         ui->spinBox_2->value() +
                         ui->spinBox_3->value() +
                         ui->spinBox_4->value() +
                         ui->spinBox_5->value());
 
-        overall += 0.25*ui->spinBox_6->value();
-        overall += 0.30*ui->spinBox_7->value();
-        overall += 0.35*ui->spinBox_8->value();
+        overall += 0.25*ui->spinBox_midterm1->value();
+        overall += 0.30*ui->spinBox_final->value();
+        overall += 0.35*ui->spinBox_project->value();
     }
     else if(ui->radioButton_2->isChecked())
     {
-        overall += 0.15*(ui->spinBox->value() +
+        overall += 0.15*(ui->spinBox_1->value() +
                         ui->spinBox_2->value() +
                         ui->spinBox_3->value() +
                         ui->spinBox_4->value() +
                         ui->spinBox_5->value());
 
-        overall += 0.50*ui->spinBox_7->value();
-        overall += 0.35*ui->spinBox_8->value();
+        overall += 0.50*ui->spinBox_final->value();
+        overall += 0.35*ui->spinBox_project->value();
     }
 
     return overall;
 }
 
-void GradeCalculator::on_spinBox_valueChanged(int arg1)
+void GradeCalculator::on_spinBox_1_valueChanged(int arg1)
 {
-    ui->horizontalSlider->setValue(arg1);
+    ui->horizontalSlider_1->setValue(arg1);
     update_overall();
 }
 
@@ -108,20 +110,20 @@ void GradeCalculator::on_spinBox_5_valueChanged(int arg1)
     update_overall();
 }
 
-void GradeCalculator::on_spinBox_6_valueChanged(int arg1)
+void GradeCalculator::on_spinBox_midterm1_valueChanged(int arg1)
 {
-    ui->horizontalSlider_6->setValue(arg1);
+    ui->horizontalSlider_midterm1->setValue(arg1);
     update_overall();
 }
 
-void GradeCalculator::on_spinBox_7_valueChanged(int arg1)
+void GradeCalculator::on_spinBox_final_valueChanged(int arg1)
 {
-    ui->horizontalSlider_7->setValue(arg1);
+    ui->horizontalSlider_final->setValue(arg1);
     update_overall();
 }
 
-void GradeCalculator::on_spinBox_8_valueChanged(int arg1)
+void GradeCalculator::on_spinBox_project_valueChanged(int arg1)
 {
-    ui->horizontalSlider_8->setValue(arg1);
+    ui->horizontalSlider_project->setValue(arg1);
     update_overall();
 }
